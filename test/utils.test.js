6831a5a1850e34fac89a0b5ee04d9ffdb41ce9ce
Yahya Gilany <yahya.gilany@live.com>
2017-07-22 19:04:51 -0400

    some unit tests for the getType util

---
+++ test/utils.test.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect;
+var getType = require('../utils').getType;
+
+describe('#getType', function () {
+  it(`should return number when passed a number`, function () {
+    var result = getType(1);
+    expect(result).to.equal('number');
+  });
+
+  it(`should return number when passed a stringified number`, function () {
+    var result = getType('1');
+    expect(result).to.equal('number');
+  });
+
+  it(`should return string when passed a string`, function () {
+    var result = getType('a string');
+    expect(result).to.equal('string');
+  });
+
+  it('should return boolean when passed a boolean value', function () {
+    var result = getType(true);
+    expect(result).to.equal('boolean');
+  });
+
+  it('should return null when passed a null value', function () {
+    var result = getType(null);
+    expect(result).to.equal('null');
+  });
+
+  it('should return object when passed a json object', function () {
+    var result = getType({"test":"test"});
+    expect(result).to.equal('object');
+  });
+});
