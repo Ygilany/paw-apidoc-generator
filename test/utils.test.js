d617fda57dc5e4d47cebcfb7872687b5ad31dfcd
Yahya Gilany <gilanyym@ucmail.uc.edu>
2019-05-30 16:37:46 -0400

    updating the utils to use the new lib

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,37 +4,32 @@ var getType = require('../utils').getType;
 describe('#getType', function () {
   it(`should return number when passed a number`, function () {
     var result = getType(1);
-    expect(result).to.equal('number');
-  });
-
-  it(`should return number when passed a stringified number`, function () {
-    var result = getType('1');
-    expect(result).to.equal('number');
+    expect(result).to.equal('Number');
   });
 
   it(`should return string when passed a string`, function () {
     var result = getType('a string');
-    expect(result).to.equal('string');
+    expect(result).to.equal('String');
   });
 
   it('should return boolean when passed a boolean value', function () {
     var result = getType(true);
-    expect(result).to.equal('boolean');
+    expect(result).to.equal('Boolean');
   });
 
   it('should return null when passed a undefined value', function () {
     var result = getType(undefined);
-    expect(result).to.equal('undefined');
+    expect(result).to.equal('Undefined');
   });
 
   it('should return null when passed a null value', function () {
     var result = getType(null);
-    expect(result).to.equal('null');
+    expect(result).to.equal('Null');
   });
 
   it('should return object when passed a json object', function () {
     var result = getType({"test":"test"});
-    expect(result).to.equal('object');
+    expect(result).to.equal('Object');
   });
 
   it('should return function when passed a function object', function () {
@@ -42,37 +37,37 @@ describe('#getType', function () {
       return;
     }
     var result = getType(aFunc);
-    expect(result).to.equal('function');
+    expect(result).to.equal('Function');
   });
 
   it('should return array when passed an array object', function () {
     var result = getType([1,2,3,4]);
-    expect(result).to.equal('array');
+    expect(result).to.equal('Array');
   });
 
   it('should return Error when passed an Error object', function () {
     var result = getType(new Error());
-    expect(result).to.equal('error');
+    expect(result).to.equal('Error');
   });
 
   it('should return Date when passed an Date object', function () {
     var result = getType(new Date());
-    expect(result).to.equal('date');
+    expect(result).to.equal('Date');
   });
 
   it('should return RegEx when passed an RegEx object1', function () {
     var result = getType(/a+c/);
-    expect(result).to.equal('regexp');
+    expect(result).to.equal('Regexp');
   });
 
   it('should return RegEx when passed an RegEx object2', function () {
     var result = getType(new RegExp('a+c'));
-    expect(result).to.equal('regexp');
+    expect(result).to.equal('Regexp');
   });
 
   it('should return symbol when passed an symbol object', function () {
     var sym = Symbol();
     var result = getType(sym);
-    expect(result).to.equal('symbol');
+    expect(result).to.equal('Symbol');
   });
 });
--- utils.js
+++ utils.js
@@ -1,86 +1,14 @@
-// This is a combination of the Core-Utils library and some modifications on my part.
-// I had some trouble using the npm packege here.
+const dataType = require(`./external_libs/get-data-type/index`);
 
 exports.getType = function (op) {
-  if (isPrimitive(op)) {
-    return isNull(op) ? 'null' :
-      isBoolean(op) ? 'boolean' :
-      isSymbol(op) ? 'symbol' :
-      isNumber(op) ? 'number' :
-      isString(op) ? 'string' :
-      isUndefined(op) ? 'undefined' : 'unknown';
-  }
-
-  if (isObject(op)) {
-    return isArray(op) ? 'array' :
-      isRegExp(op) ? 'regexp' :
-      isDate(op) ? 'date' :
-      isError(op) ? 'error' : 'object';
-  }
-
-  if (isFunction(op)) return 'function';
+  return dataType(op).replace(/^\w/, c => c.toUpperCase());
 };
 
-function isPrimitive(arg) {
-  return arg === null ||
-         typeof arg === 'boolean' ||
-         typeof arg === 'number' ||
-         typeof arg === 'string' ||
-         typeof arg === 'symbol' ||  // ES6 symbol
-         typeof arg === 'undefined';
-}
-
-function isNull(arg) {
-  return arg === null;
-}
-
-function isBoolean(arg) {
-  return typeof arg === 'boolean';
-}
-
-function isSymbol(arg) {
-  return typeof arg === 'symbol';
-}
-
-function isNumber(arg) {
-  return typeof arg === 'number' || parseInt(arg, 10);
-}
-
-function isString(arg) {
-  return typeof arg === 'string';
-}
-
-function isUndefined(arg) {
-  return arg === undefined;
-}
-
-function isObject(arg) {
-  return typeof arg === 'object' && arg !== null;
-}
-
-function isArray(arg) {
-  if (Array.isArray) {
-    return Array.isArray(arg);
-  }
-  return objectToString(arg) === '[object Array]';
-}
-
-function isRegExp(re) {
-  return objectToString(re) === '[object RegExp]';
-}
-
-function isDate(d) {
-  return objectToString(d) === '[object Date]';
-}
-
-function isError(e) {
-  return (objectToString(e) === '[object Error]' || e instanceof Error);
-}
-
-function isFunction(arg) {		
-  return typeof arg === 'function';		
-}
-
-function objectToString(o) {		
-  return Object.prototype.toString.call(o);		
+exports.toCamelCase = function (sentenceCase) {
+  var out = "";
+  sentenceCase.split(" ").forEach(function (el, idx) {
+      var add = el.toLowerCase();
+      out += (idx === 0 ? add : add[0].toUpperCase() + add.slice(1));
+  });
+  return out;
 }
